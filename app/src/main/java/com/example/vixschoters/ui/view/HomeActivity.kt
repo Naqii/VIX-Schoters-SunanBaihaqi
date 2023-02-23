2196e560dd84841d20b71983aef8bc5bb193f571
Naqi <sunanhaqi.sh@gmail.com>
2023-02-23 11:29:58 +0700

    1.1

---
+++ app/src/main/java/com/example/vixschoters/ui/view/HomeActivity.kt
@@ -0,0 +1,20 @@
+package com.example.vixschoters.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vixschoters.databinding.ActivityHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
